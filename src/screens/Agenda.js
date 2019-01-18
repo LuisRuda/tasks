3555bdc032be26f1add6cc48336a55692ae506dd
Luis Paulo Ruda <luispauloruda@gmail.com>
2019-01-17 22:14:43 -0200

    Armazenar dados com AsyncStorage

--- src/screens/Agenda.js
+++ src/screens/Agenda.js
@@ -6,7 +6,8 @@ import {
     ImageBackground,
     FlatList,
     TouchableOpacity,
-    Platform
+    Platform,
+    AsyncStorage
 } from 'react-native'
 import moment from 'moment'
 import 'moment/locale/pt-br'
@@ -19,32 +20,7 @@ import AddTask from './AddTask'
 
 export default class Agenda extends Component {
     state = {
-        tasks: [
-            { id: Math.random(), desc: 'Comprar o Curso React Native',
-                estimateAt: new Date(), doneAt: new Date() },
-            { id: Math.random(), desc: 'Concluir o curso',
-                estimateAt: new Date(), doneAt: null},
-            { id: Math.random(), desc: 'Comprar o Curso React Native',
-                estimateAt: new Date(), doneAt: new Date() },
-            { id: Math.random(), desc: 'Concluir o curso',
-                estimateAt: new Date(), doneAt: null},
-            { id: Math.random(), desc: 'Comprar o Curso React Native',
-                estimateAt: new Date(), doneAt: new Date() },
-            { id: Math.random(), desc: 'Concluir o curso',
-                estimateAt: new Date(), doneAt: null},
-            { id: Math.random(), desc: 'Comprar o Curso React Native',
-                estimateAt: new Date(), doneAt: new Date() },
-            { id: Math.random(), desc: 'Concluir o curso',
-                estimateAt: new Date(), doneAt: null},
-            { id: Math.random(), desc: 'Comprar o Curso React Native',
-                estimateAt: new Date(), doneAt: new Date() },
-            { id: Math.random(), desc: 'Concluir o curso',
-                estimateAt: new Date(), doneAt: null},
-            { id: Math.random(), desc: 'Comprar o Curso React Native',
-                estimateAt: new Date(), doneAt: new Date() },
-            { id: Math.random(), desc: 'Concluir o curso',
-                estimateAt: new Date(), doneAt: null},
-        ],
+        tasks: [],
         visibleTasks: [],
         showDoneTasks: true,
         showAddTask: false
@@ -76,6 +52,7 @@ export default class Agenda extends Component {
             visibleTasks = this.state.tasks.filter(pending)
         }
         this.setState({ visibleTasks })
+        AsyncStorage.setItem('tasks', JSON.stringify(this.state.tasks))
     }
 
     toggleFilter = () => {
@@ -83,8 +60,10 @@ export default class Agenda extends Component {
                 , this.filterTasks)
     }
 
-    componentDidMount = () => {
-        this.filterTasks()
+    componentDidMount = async () => {
+        const data = await AsyncStorage.getItem('tasks')
+        const tasks = JSON.parse(data) || []
+        this.setState({ tasks }, this.filterTasks)
     }
 
     toggleTask = id => {
